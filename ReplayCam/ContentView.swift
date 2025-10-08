acc4176770e0671e5df59b48a3415f18cd058362
Peter <PeterWang@tiss.org.tw>
2025-10-08 11:17:32 +0800

    Initial Commit

---
+++ ReplayCam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReplayCam
+//
+//  Created by 王柏舜 on 2025/10/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReplayCam/ReplayCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplayCamApp.swift
+//  ReplayCam
+//
+//  Created by 王柏舜 on 2025/10/8.
+//
+
+import SwiftUI
+
+@main
+struct ReplayCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
